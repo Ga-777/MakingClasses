4aa7bdfa913ee3c0696a79ab6d09ecae3c93cea1
362579823 <362579823@2260I-L29-030>
2026-09-16 13:19:26 -0400

    0007

--- MakingClasses/Program.cs
+++ MakingClasses/Program.cs
@@ -1,20 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MakingClasses;
 
-Console.WriteLine("Hello, World!");
-for (int i = 0; i < 1; i++)
+Console.WriteLine("Press Enter to Roll!");
+for (int i = 0; i < 900000000; i++)
 {
+    if (i <= 90)
+    {
+        Console.ReadLine();
+    }
+    
+    
+
+
+    
     Die Die1 = new Die(), Die2 = new Die();
     Die1.RollDie();
-    Console.WriteLine(Die1);
+    Die2.RollDie();
 
+    Console.WriteLine(Die1);
+    Die1.ColoredDie();
     Die1.DrawRoll();
+
+
+
     Console.WriteLine(Die2.Roll);
+    Die2.ColoredDie();
     Die2.DrawRoll();
-    if (Die1 == Die2)
+    if (Die1.Roll == Die2.Roll)
     {
-        Console.WriteLine(Die1 + " is the same as " + Die2);
+        Console.WriteLine();
+        Console.WriteLine("Die1 is the same as Die2.");
+        Console.WriteLine();
     }
+    if (Die1.Roll > Die2.Roll)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Die1 is more then Die2.");
+        Console.WriteLine();
+    }
+    if (Die1.Roll < Die2.Roll)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Die2 is more then Die1.");
+        Console.WriteLine();
+    }
+    else if (Die1.Roll == 1 && Die2.Roll == 1)
+    { 
+      Console.WriteLine();
+
+
+      Console.WriteLine("SNAKE EYES!");
+    
+    }
+    int sumofSeven = Die1.Roll + Die2.Roll;
+    if (sumofSeven == 7)
+    {
+        Console.WriteLine("Both Dies add up to 7");
+    }
+
+    
+
+
+    
 }
 
 //Die1.Roll = 5;
--- MakingClasses/die.cs
+++ MakingClasses/die.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,14 @@ namespace MakingClasses
         private int _sides;
         private int _roll;
         private Random _generator;
+        private ConsoleColor _color;
 
         public Die()
         { 
             _generator = new Random();
             _sides = 6;
             _roll = _generator.Next(1, _sides + 1);
-        
+            _color = ConsoleColor.White;
         
         
         }
@@ -43,68 +45,111 @@ namespace MakingClasses
         {
             _roll = _generator.Next(1 , _sides + 1);
         }
-        public void DrawRoll()
+        
+        public ConsoleColor _Color
+        { 
+            get { return _color; } 
+            set { _color = value; }
+        }
+        public void ColoredDie()
         {
+            
             if (Roll == 1)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("-----");
-                Console.WriteLine("|   |");
-                Console.WriteLine("| 0 |");
-                Console.WriteLine("|   |");
-                Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                _color = ConsoleColor.Red;
             }
-            else if(Roll == 2)
+            else if (Roll == 2)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("-----");
-                Console.WriteLine("|0  |");
-                Console.WriteLine("|   |");
-                Console.WriteLine("|  0|");
-                Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                _color= ConsoleColor.Green;
             }
             else if (Roll == 3)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
+                _color = ConsoleColor.Blue;      
+            }
+            else if (_roll == 4)
+            {
+                _color = ConsoleColor.Yellow;
+            }
+            else if (_roll == 5)
+            {
+                _color = ConsoleColor.Magenta;
+            }
+            else if (_roll == 6)
+            {
+                _color = ConsoleColor.Cyan;
+            }
+            else
+            {
+                _color = ConsoleColor.White;
+            }
+            Console.ForegroundColor = _color;
+        }
+        public void DrawRoll()
+        {
+            ConsoleColor currentForecolor = Console.ForegroundColor;
+            currentForecolor = ConsoleColor.White;
+
+            if (Roll == 1)
+            {
+                //Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| 0 |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+                //Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if(Roll == 2)
+            {
+                //Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("-----");
+                Console.WriteLine("|0  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  0|");
+                Console.WriteLine("-----");
+                //Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (Roll == 3)
+            {
+                //Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("-----");
                 Console.WriteLine("|0  |");
                 Console.WriteLine("| 0 |");
                 Console.WriteLine("|  0|");
                 Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                //Console.ForegroundColor = ConsoleColor.White;
             }
             else if (Roll == 4)
             {
-                Console.ForegroundColor = ConsoleColor.Magenta;
+                //Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("-----");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("|   |");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                //Console.ForegroundColor = ConsoleColor.White;
             }
             else if (Roll == 5)
             {
-                Console.ForegroundColor = ConsoleColor.Blue;
+                //Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("-----");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("| 0 |");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                //Console.ForegroundColor = ConsoleColor.White;
             }
             else if (Roll == 6)
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
+               // Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("-----");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("|0 0|");
                 Console.WriteLine("-----");
-                Console.ForegroundColor = ConsoleColor.White;
+                //Console.ForegroundColor = ConsoleColor.White;
             }
+            Console.ForegroundColor = currentForecolor;
         }
 
     }
